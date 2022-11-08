93827e985618815b6adbcb2a98533383c29fc11e
john.doe <robert.hak@tul.cz>
2022-11-08 22:13:14 +0100

    on the implementation of better djikstras method yay

--- ALDCodeCriticStuff/Solutions/GPSStuff/DijkstrasSolver.cs
+++ ALDCodeCriticStuff/Solutions/GPSStuff/DijkstrasSolver.cs
@@ -2,5 +2,43 @@ namespace ALDCodeCriticStuff.Solutions.GPSStuff;
 
 public class DijkstrasSolver
 {
+    public Vertex CurrentVertex { get; set; }
     
+    private DijkstrasSolver(Vertex startVertex)
+    {
+        this.CurrentVertex = startVertex;
+    }
+    
+    public static DijkstrasSolver Create(int startingCityIndex)
+    {
+        return new DijkstrasSolver(new Vertex(startingCityIndex, null, 0));
+    }
+
+    public int AddNextVertex(int idOfNextCity, ulong distance)
+    {
+        try
+        {
+            var vertex = new Vertex(idOfNextCity, this.CurrentVertex, distance);
+            this.CurrentVertex = vertex;
+        }
+        catch (Exception)
+        {
+            return 2;
+        }
+        return 0;
+    }
+    
+    
+    // thankfully this way of getting the total distance is O(n)
+    public ulong GetTotalDistance()
+    {
+        var tempVertex = this.CurrentVertex;
+        ulong totalDistance = 0;
+        while(tempVertex.PreviousVertex != null)
+        {
+            totalDistance += tempVertex.DistanceToPreviousVertex;
+            tempVertex = tempVertex.PreviousVertex;
+        }
+        return totalDistance;
+    }
 }
---
+++ ALDCodeCriticStuff/Solutions/GPSStuff/Vertex.cs
@@ -0,0 +1,23 @@
+// ReSharper disable InconsistentNaming
+namespace ALDCodeCriticStuff.Solutions.GPSStuff;
+
+public class Vertex
+{
+    public int Id { get; set; } // id of the city
+    
+    public Vertex? PreviousVertex { get; set; }
+    public ulong DistanceToPreviousVertex { get; set; }
+    
+    // true connection to the Next Vertex exists at such id (id = index of the array)
+    //public bool[] NextVertexIds { get; set; }
+    
+    public Vertex(int id, Vertex? previousVertex, ulong distanceToPreviousVertex)
+    {
+        Id = id;
+        PreviousVertex = previousVertex;
+        DistanceToPreviousVertex = distanceToPreviousVertex;
+        //NextVertexIds = new bool[CityData.Cities.Count];
+    }
+    
+    
+}
