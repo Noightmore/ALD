08bba8859126d323c530ca5ff545a2657103ec98
john.doe <robert.hak@tul.cz>
2023-02-02 12:27:42 +0100

    shrug

--- TGHStuff/TranspoziceGrafu/cmake-build-debug/cmake_install.cmake
+++ TGHStuff/TranspoziceGrafu/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/rob/Programming/Csharp/ALD/TGHStuff/Solutions/TranspoziceGrafu
+# Install script for directory: /home/rob/Programming/Csharp/ALD/TGHStuff/TranspoziceGrafu
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/rob/Programming/Csharp/ALD/TGHStuff/Solutions/TranspoziceGrafu/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/rob/Programming/Csharp/ALD/TGHStuff/TranspoziceGrafu/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
